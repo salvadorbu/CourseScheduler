04fbdc0e74c812ce9e1e582d12465df630b10f3a
Salvador Buenadicha Calvo <salvadorbu@vt.edu>
2023-09-16 12:31:42 -0400

    update

--- src/main/java/com/example/CourseScheduler/RegistrationTimetableTest.java
+++ src/main/java/com/example/CourseScheduler/RegistrationTimetableTest.java
@@ -1,7 +1,25 @@
 package com.example.CourseScheduler;
 
-import org.testng.annotations.Test;
-import org.junit.jupiter.api.Assertions.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+import java.io.IOException;
+
 
 public class RegistrationTimetableTest {
+    private RegistrationTimetable rt;
+    @Before
+    public void setUp() {
+        try {
+            rt = new RegistrationTimetable("202309");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testSampleRequest() throws IOException {
+        rt.getCourses("MATH", "2114");
+    }
 }
