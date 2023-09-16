5c6519cc0efa8e95f1f712af051c8c03a4048a43
Salvador Buenadicha Calvo <salvadorbu@vt.edu>
2023-09-15 23:32:20 -0400

    Initial commit

---
+++ src/main/java/com/example/CourseScheduler/CourseSchedulerApplication.java
@@ -0,0 +1,20 @@
+package com.example.CourseScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class CourseSchedulerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSchedulerApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
---
+++ src/test/java/com/example/CourseScheduler/CourseSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CourseScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
